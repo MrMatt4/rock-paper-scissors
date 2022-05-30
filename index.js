ebf78a1f011ffb495f278f8b373181cc71c15938
Matei Dinca <mateidinca@Mateis-MacBook-Pro.local>
2022-05-30 21:58:01 +0300

    Initial commit 3
    Signed-off-by: Matei Dinca <mateidinca@Mateis-MacBook-Pro.local>

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
+let computerValue;
+
 function computerPlay() {
     let computerHand = Math.random();
+    
     switch (true) {
         case (computerHand < 0.33):
-            console.log("Rock");
+            computerValue = "Rock";
+            console.log("Computer plays: Rock");
             break;
         case (computerHand > 0.66):
-            console.log("Scissors");
+            computerValue = "Scissors";
+            console.log("Computer plays: Scissors");
             break;
         default:
-            console.log("Paper");
+            computerValue = "Paper";
+            console.log("Computer plays: Paper");
             break;
     }
- return computerHand;   
+ return computerValue;   
 
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection.includes("rock") && computerSelection.includes("scissors")) {
+        return "You Win! Rock beats Scissors";
+    }
+    else if (playerSelection.includes("rock") && computerSelection.includes("paper")) {
+        return "You Lose! Paper beats Rock";
+    }
+    else {
+        return "You Draw! Both drew Rock";
+    }
+}
+
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
