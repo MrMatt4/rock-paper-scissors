1625de0e1941315244ff5b78dbedffa927bc1817
Matei Dinca <mateidinca@Mateis-MacBook-Pro.local>
2022-05-30 21:39:06 +0300

    Initial commit 2
    Signed-off-by: Matei Dinca <mateidinca@Mateis-MacBook-Pro.local>

--- index.js
+++ index.js
@@ -11,7 +11,19 @@ function computerPlay() {
             console.log("Paper");
             break;
     }
+
+    if (computerHand < 0.33) {
+        console.log(" less than 0.33");
+    }
+    else if (computerHand > 0.66) {
+        console.log(" more than 0.66");
+    }
+    else {
+        console.log(computerHand);
+    }
  return computerHand;   
+
+ 
 }
 
 console.log(computerPlay());
